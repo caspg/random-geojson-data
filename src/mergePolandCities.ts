b6c6eea213139fb42463750851317dea1194b497
Kacper Golinski <kacper.golinski@gmail.com>
2023-11-26 18:57:43 +0100

    update mergePolandCities script

--- src/mergePolandCities.ts
+++ src/mergePolandCities.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import simplify from '@turf/simplify'
 
 const TARGET_DIR = 'geojson/poland/cities'
 const OUTPUT_FILE = 'geojson/poland/poland_cities_simplified.geojson'
@@ -6,17 +7,28 @@ const OUTPUT_FILE = 'geojson/poland/poland_cities_simplified.geojson'
 const filenames = fs.readdirSync(TARGET_DIR)
 const geojsonFeatures = filenames.flatMap((filename) => {
   console.log(`Reading ${filename}`)
-  const file = fs.readFileSync(`${TARGET_DIR}/${filename}`, 'utf-8')
 
+  const file = fs.readFileSync(`${TARGET_DIR}/${filename}`, 'utf-8')
   const geojson = JSON.parse(file)
 
   return geojson.features
 })
 
+const simplifiedFeatures = geojsonFeatures.map((feature) => {
+  const slimedFeature = {
+    ...feature,
+    properties: {
+      name: feature.properties.name,
+    },
+  }
+
+  return simplify(slimedFeature, { highQuality: true, tolerance: 0.0005 })
+})
+
 const featureCollection = {
   type: 'FeatureCollection',
-  features: geojsonFeatures,
+  features: simplifiedFeatures,
 }
 
-fs.writeFileSync(OUTPUT_FILE, JSON.stringify(featureCollection, null, 2))
+fs.writeFileSync(OUTPUT_FILE, JSON.stringify(featureCollection))
 console.log('All done!')
